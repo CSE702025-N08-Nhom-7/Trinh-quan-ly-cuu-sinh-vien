9d47cdd3216a82784e3b37ecf8cb17c4420adc32
MinhNgyen <minhharu1998@gmail.com>
2025-06-20 00:25:38 +0700

    Push mã nguồn lên repo mới

---
+++ about_us.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>About Us</title>
+<link rel="stylesheet" href="css/about_us.css" />
+
+<?php
+include_once"setting/aboutus_navigation.php";
+?>
+</head>
+
+<body>
+<div id="history">
+<p class="p1">Hội Quản lý Cựu sinh viên của Đại Học Phenikaa</p>
+<p class="p3">Hiệp hội Cựu sinh viên Phenikaa chịu trách nhiệm giám sát các chương trình đào tạo sau đại học tại Phenikaa UNI, bao gồm quản lý tuyển sinh, bổ nhiệm cựu sinh viên, đánh giá thành viên, cấp hỗ trợ tài chính và thực hiện các hoạt động kiểm tra, đánh giá. </p><br />
+<p class="p2">CHỨC NĂNG CỦA HỆ THỐNG QUẢN LÝ CỰU SINH VIÊN (AMS)</p>
+<p class="p3"> Hệ thống AMS hỗ trợ các chức năng sau đây:
+<ul class="ul2">
+	<li>Quảng bá các chương trình sau đại học của Phenikaa UNI</li>
+	<li>Hỗ trợ việc bổ nhiệm/vị trí công tác cho cựu sinh viên sau khi tốt nghiệp</li>
+	<li>Giám sát việc cung cấp hỗ trợ tài chính</li>
+	<li>Tổ chức các sự kiện dành cho cựu sinh viên</li>
+	<li>Tạo điều kiện giao tiếp giữa các cựu sinh viên sau khi ra trường</li>
+	<li>Phối hợp công tác đánh giá phục vụ mục đích tốt nghiệp</li>
+	<li>Tổ chức các hoạt động nhằm nâng cao năng lực nghiên cứu của học viên sau đại học</li>
+
+</ul>
+</p>
+</div>
+</body>
+</html>
---
+++ contact_us.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Contact Us</title>
+<link rel="stylesheet" href="css/contact_us.css" />
+
+<?php
+include_once"setting/contactus_navigation.php";
+?>
+</head>
+
+<body>
+<div id="contact">
+<p class="p1">Liên hệ với chúng tôi</p>
+<p class="p2">University</p>
+<p class="p3">Yên Nghĩa, Hà Đông, Hà Nội<br>
+Nguyen Trac Street<br>
+Yen Nghia Ward<br>
+Ha Dong District, Hanoi City Hà Đông, Vietnam 10000<br><br></p>
+<table>
+<tr>
+	<td>
+			<p class="p2">Liên hệ với chúng tôi:</p>
+			<p class="p3">Tel : +XXXX XXX XXX</p>
+			<p class="p3">Fax: +XXX XXX XXX </p>
+	</td>		
+	<td >
+    <p class="p2">Email:</p>truyenthong@phenikaa-uni.edu.vn<p class="p3"></p><br /><br />
+    <p class="p2">Các trang thông tin điện tử của chúng tôi:</p>
+  <p class="p3"> <a href="https://www.facebook.com/daihocphenikaa">
+   <img src="pictures/facebook.png" width="40" height="40"></a> 
+   <a href="https://www.instagram.com/dh_phenikaa/"> 
+   <img src="pictures/instagram.png" width="55" height="40">
+  </a> <a href="https://x.com/elonmusk">
+  <img src="pictures\x.png" width="35" height="35"></a></p><br /><br />
+    </td>
+</tr> 
+</table>
+
+</div>
+<br><br><br>
+<br /><hr color="#050119" size="4"/>
+<br>
+<div id="faq">
+<p class="p1">FAQ</p>
+<p class="p2"><br>Bạn có biết ai là cựu sinh viên Phenikaa không?</p>
+<p class="p3">Tất cả các cá nhân đã theo học chương trình Cao đẳng, Đại học, Sau đại học hoặc Tiến sĩ tại Trường Đại Học Phenikaa đều có thể đăng ký trở thành cựu sinh viên của chúng tôi.</p>
+<p class="p2"><br>Làm thế nào để tôi trở thành thành viên của Hội cựu Sinh viên Trường Đại học Phenikaa?</p>
+<p class="p3">Tất cả sinh viên bậc Đại học và Sau đại học của Trường Đại học Phenikaa đều có thể đăng ký trở thành cựu sinh viên của chúng tôi. Việc cập nhật thông tin cá nhân vào Cơ sở dữ liệu Cựu sinh viên Phenikaa là rất quan trọng. Bạn có thể thực hiện điều này bằng cách đăng ký thành viên trực tuyến và thanh toán tại quầy của Văn phòng Cựu sinh viên PKA, để chúng tôi có thể giữ liên lạc với bạn suốt đời.</p>
+<p class="p2"><br>Có tốn chi phí gì khi trở thành thành viên không?</p>
+
+<p class="p3">Có, nhưng chỉ áp dụng cho việc đăng ký thành viên.<br>
+Bạn có thể lựa chọn đóng phí theo năm hoặc phí thành viên trọn đời.<br><br>
+Mức phí cụ thể như sau:<br>
+Thành viên theo năm: 300.000 VNĐ<br>
+Thành viên trọn đời: 500.000 VNĐ </p>
+<p class="p2"><br>Với tư cách là cựu sinh viên, tôi có thể đóng góp cho nhà trường bằng cách nào? </p>
+<p class="p3">Bạn có thể đóng góp bằng cách tham gia vào các hoạt động khác nhau do nhà trường tổ chức hoặc đóng góp ý tưởng, chương trình nhằm nâng cao trải nghiệm học tập cho tất cả sinh viên.<br>
+Những hình thức hỗ trợ khác có thể bao gồm: tài trợ, quyên góp, chia sẻ chuyên môn hoặc tham gia các hoạt động tình nguyện.<br>
+Dù là đóng góp theo cách nào, chúng tôi đều vô cùng trân trọng và chào đón!</p>
+<p class="p2"><br>Tôi vẫn đang là sinh viên của trường, vậy tôi có thể tham gia các hoạt động của cựu sinh viên không?</p>
+<p class="p3">Vâng, tất nhiên rồi! Chúng tôi tin rằng mối liên kết giữa cựu sinh viên và sinh viên nên bắt đầu ngay từ khi bạn bước chân vào trường, bởi sau này chính bạn sẽ trở thành một phần trong cộng đồng cựu sinh viên của chúng tôi.<br>
+Văn phòng Cựu sinh viên cũng cam kết tổ chức các chương trình đào tạo, đặc biệt là về kỹ năng mềm và phát triển nghề nghiệp, nhằm nâng cao khả năng cạnh tranh của bạn trên thị trường lao động sau khi tốt nghiệp.<br>
+Chúng tôi hy vọng điều này sẽ khơi dậy niềm tự hào về mái trường Phenikaa trong bạn, và sau này bạn sẽ mong muốn đóng góp để cùng chung tay xây dựng và phát triển ngôi trường ngày càng vững mạnh.
+
+</p>
+</div>
+<br><br><br><br>
+</body>
+</html>
